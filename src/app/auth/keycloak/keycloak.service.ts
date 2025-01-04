8e01e10869b627e3e43cabae15077bc18ae65144
danielmedina@synapticlinks.com.ar <danielmedina@synapticlinks.com.ar>
2025-01-03 23:47:19 -0300

    fix domain name

--- src/app/auth/keycloak/keycloak.service.ts
+++ src/app/auth/keycloak/keycloak.service.ts
@@ -14,7 +14,7 @@ export class KeycloakService {
     if (!this._keycloak){
       this._keycloak = new Keycloak({
         
-        url: "http://warriorit.site:8080/",
+        url: "http://warriorit.site/auth",
         realm: 'book-social-network',
         clientId: 'bsn'
       });
--- src/app/service/config.ts
+++ src/app/service/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-    apiUrl: 'http://149.50.130.196:8088',
+    apiUrl: 'http://www.warriorit.site/',
     keycloakUrl: 'http:keyloak-sm:8080'
 }
