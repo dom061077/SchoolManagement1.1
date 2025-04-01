3514c1a062e7dd4b7b98848f673214829d2c440c
danielmedina@synapticlinks.com.ar <danielmedina@synapticlinks.com.ar>
2025-04-01 00:33:20 -0300

    profile store

--- src/app/user-profile/store/profile.actions.ts
+++ src/app/user-profile/store/profile.actions.ts
@@ -1,2 +1,10 @@
 import { createAction, props } from '@ngrx/store';
-import {  } from 
+import { UserProfile } from '../user-profile.model';
+
+export const LOAD_PROFILE = '[Profile] Load Profile';
+
+export const loadProfile = createAction(LOAD_PROFILE,
+    props<{profile: UserProfile}>()
+);
+
+
