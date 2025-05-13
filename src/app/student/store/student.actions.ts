6f9a39784720974597c7554f26c55d95446ac9e8
danielmedina@synapticlinks.com.ar <danielmedina@synapticlinks.com.ar>
2025-05-12 23:54:07 -0300

    student actions

---
+++ src/app/student/store/student.actions.ts
@@ -0,0 +1,6 @@
+
+
+
+export const LOAD_STUDENT='[STUDENT page]load STUDENT';
+export const LOAD_STUDENT_SUCCESS='[STUDENT page]load STUDENT success';
+export const LOAD_STUDENT_FAIL='[STUDENT page]load STUDENT fail';
