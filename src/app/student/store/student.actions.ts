49d42a0b615c3788c2a1f8e381193245227fe3dc
danielmedina@synapticlinks.com.ar <danielmedina@synapticlinks.com.ar>
2025-05-18 21:03:58 -0300

    fix in student module code

--- src/app/student/store/student.actions.ts
+++ src/app/student/store/student.actions.ts
@@ -1,3 +1,4 @@
+import { createAction, props } from "@ngrx/store";
 
 
 
@@ -9,3 +10,17 @@ export const ADD_STUDEN='[STUDENT page]add STUDENT';
 export const ADD_STUDEN_SUCCESS='[STUDENT page]add STUDENT success';
 export const UPDATE_STUDENT='[STUDENT page]update STUDENT';
 export const UPDATE_STUDENT_SUCCESS='[STUDENT page]update STUDENT success';
+export const DELETE_STUDENT='[STUDENT page]delete STUDENT';
+export const DELETE_STUDENT_SUCCESS='[STUDENT page]delete STUDENT success';
+export const GET_STUDENT='[STUDENT page]get STUDENT';
+export const GET_STUDENT_SUCCESS='[STUDENT page]get STUDENT success';
+
+
+export const loadSTUDENT=createAction(LOAD_STUDENT,props<{offset:number,limit: number, qfilter:string, sorts:string}>());
+export const loadSTUDENTsuccess=createAction(LOAD_STUDENT_SUCCESS,props<{list:any[], totalCount: number}>());
+export const loadSTUDENTfail=createAction(LOAD_STUDENT_FAIL,props<{errormessage:string}>());
+export const loadSTUDENTtotalrows=createAction(LOAD_STUDENT_TOTALROWS,props<{totalRows: number}>());
+export const addSTUDENT=createAction(ADD_STUDEN,props<{inputdata:any}>());
+export const addSTUDENTsuccess=createAction(ADD_STUDEN_SUCCESS,props<{inputdata:any}>());
+export const updateSTUDENT=createAction(UPDATE_STUDENT,props<{inputdata:any}>());
+export const updateSTUDENTsuccess=createAction(UPDATE_STUDENT_SUCCESS,props<{inputdata:any}>());
--- src/app/student/studentlisting/studentlisting.component.ts
+++ src/app/student/studentlisting/studentlisting.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 //import { MatDialog } from '@angular/material/dialog';
@@ -13,12 +12,14 @@ import { Subscription } from 'rxjs';
 })
 export class StudentlistingComponent implements OnInit, OnDestroy {
   displayedColumns: string[] = ['id', 'name', 'age', 'grade', 'actions'];
-  dataSource = new MatTableDataSource<Student>([]);
+  dataSource : any;
+  errormessage : string = '';
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  
   filterForm: FormGroup;
   private subscriptions: Subscription[] = [];
 
-  constructor(private store: Store<AppState>, private fb: FormBuilder) {
+  constructor(private store: Store, private fb: FormBuilder) {
     this.filterForm = this.fb.group({
       name: [''],
       age: [''],
@@ -28,11 +29,11 @@ export class StudentlistingComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.loadStudents();
-    this.subscriptions.push(
+    /*this.subscriptions.push(
       this.store.select(selectStudents).subscribe((students) => {
         this.dataSource.data = students;
       })
-    );
+    );*/
   }
 
   ngOnDestroy(): void {
@@ -40,12 +41,12 @@ export class StudentlistingComponent implements OnInit, OnDestroy {
   }
 
   loadStudents() {
-    this.store.dispatch(loadStudents());
+    //this.store.dispatch(loadStudents());
   }
 
   applyFilter() {
     const filterValue = this.filterForm.value;
-    this.store.dispatch(filterStudents({ filterValue }));
+    //this.store.dispatch(filterStudents({ filterValue }));
   }
 
   clearFilter() {
@@ -53,4 +54,17 @@ export class StudentlistingComponent implements OnInit, OnDestroy {
     this.loadStudents();
   }
 
+  studentEdit(id: number) {
+    // Open edit dialog
+  }
+  
+  studentDelete(id: number) {
+    // Open delete confirmation dialog
+  }
+
+  sortData(event: any) {
+    const sortField = event.active;
+    const sortDirection = event.direction;
+    //this.store.dispatch(sortStudents({ sortField, sortDirection }));
+  }
 }
