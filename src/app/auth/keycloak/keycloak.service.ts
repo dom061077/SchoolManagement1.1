5d0bd417bc992cfc812c08a7166ccf982d5fb19c
dom061077@gmail.com <dom061077@gmail.com>
2025-05-19 12:58:35 -0300

    fix keycloak url api

--- src/app/auth/keycloak/keycloak.service.ts
+++ src/app/auth/keycloak/keycloak.service.ts
@@ -15,7 +15,7 @@ export class KeycloakService {
     if (!this._keycloak){
       this._keycloak = new Keycloak({
         
-        url: 'http://keycloak-sm:8080/',//"https://www.warriorit.site/",//url: "http://warriorit.site:8080/",
+        url: 'https://www.warriorit.site/',//" http://keycloak-sm:8080/",//url: "http://warriorit.site:8080/",
         realm: 'book-social-network',
         clientId: 'bsn'
       });
