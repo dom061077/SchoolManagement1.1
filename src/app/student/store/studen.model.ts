cf471e57dcf98cec53921715192f54f0ee7eba82
danielmedina@synapticlinks.com.ar <danielmedina@synapticlinks.com.ar>
2025-08-18 00:39:57 -0300

    student module

---
+++ src/app/student/store/studen.model.ts
@@ -0,0 +1,84 @@
+import { StudentModule } from "../student.module";
+
+/*
+    String apellido
+    String nombre
+    String apellidoNombre
+    java.sql.Date fechaNacimiento
+
+    int dni
+    String cuil
+    String direccion
+    boolean planSocial=false
+    boolean trabaja=false
+    
+    String apellidoTutor
+    String nombreTutor
+    EstudioEnum estudioPrimarioTutor
+    EstudioEnum estudioSecundarioTutor
+    EstudioEnum estudioTerUnivTutor
+    
+    int dniTutor
+    String cuilTutor
+    String telefono1
+    String telefono2
+    boolean fotoDni=false
+    boolean constanciaCuil=false
+    boolean constancia6grado=false
+    boolean actaNacimiento=false
+    boolean constanciaRegular=false
+    boolean foto4x4=false
+    boolean fotoCarnetVac=false
+    boolean fichaMedica=false
+    boolean aptitudFisica=false
+    boolean grupoSanguineo=false
+    boolean fichaInscripcion=false
+    boolean libreta6grado=false
+    boolean fotocopiaLibroMatriz=false
+    boolean fotocopiaDniTutor=false
+    boolean constanciaCuilTutor=false
+    
+    Localidad localidad
+    ParentescoTutor parentescoTutor
+
+*/
+export interface Student {
+    id: number;
+    apellido: string;
+    nombre: string;
+    apellidoNombre: string;
+    fechaNacimiento: Date;
+    dni: number;
+    cuil?: string;
+    direccion?: string;
+    planSocial?: boolean;
+    trabaja?: boolean;      
+    apellidoTutor?: string;
+    nombreTutor?: string;
+    estudioPrimarioTutor?: string;
+    estudioSecundarioTutor?: string;
+    estudioTerUnivTutor?: string;
+    dniTutor?: number;
+    cuilTutor?: string;
+    telefono1?: string;
+    telefono2?: string;
+    fotoDni?: boolean;
+    constanciaCuil?: boolean;
+    constancia6grado?: boolean;
+    actaNacimiento?: boolean;
+    constanciaRegular?: boolean;
+    foto4x4?: boolean;
+    fotoCarnetVac?: boolean;
+    fichaMedica?: boolean;
+    aptitudFisica?: boolean;    
+    grupoSanguineo?: boolean;
+    fichaInscripcion?: boolean;
+    libreta6grado?: boolean;
+    fotocopiaLibroMatriz?: boolean;
+    fotocopiaDniTutor?: boolean;
+    constanciaCuilTutor?: boolean;
+    localidad?: string; // Assuming Localidad is a string for simplicity
+    parentescoTutor?: string; // Assuming ParentescoTutor is a string for simplicity
+    
+
+}
--- src/app/student/store/student.state.ts
+++ src/app/student/store/student.state.ts
@@ -1,6 +1,20 @@
-import { StudentModule } from "../student.module";
 
+import { createEntityAdapter, EntityState } from '@ngrx/entity';
+import { Student } from '../student.model';
 
-export interface StudentState {
-    
+export interface StudentState extends EntityState<Student> {
+    errormessage: string;
+    isloading: boolean;
+    totalRows: number;
 }
+
+export const studentAdapter = createEntityAdapter<Student>({    
+    selectId: (student: Student) => student.id,
+});
+
+export const StudentState: StudentState = studentAdapter.getInitialState({
+    errormessage: '',
+    isloading: false,
+    totalRows: 0
+});
+
