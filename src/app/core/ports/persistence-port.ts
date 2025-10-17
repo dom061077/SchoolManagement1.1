bb5c0ca405245c76d921c7cc47d6fd1c30b24dd7
danielmedina@synapticlinks.com.ar <danielmedina@synapticlinks.com.ar>
2025-10-17 00:13:07 -0300

    student effects

--- src/app/core/ports/persistence-port.ts
+++ src/app/core/ports/persistence-port.ts
@@ -2,7 +2,8 @@ import { Observable } from "rxjs";
 
 
 export interface IPersistencePort<T, ID = number, Q = string, S = string>{
-    list(query: string): Observable<T[]>;
+    list(offset: number, limit: number, query: string, qfilter: string, qsort: string): Observable<T[]>;
+    getById(id: ID): Observable<T>;
     save(entity: T): Observable<T>;
     update(id: ID, entity: T): Observable<T>;
     delete(id: ID): Observable<void>;
--- src/app/infra/store/student.effects.ts
+++ src/app/infra/store/student.effects.ts
@@ -5,18 +5,25 @@ import { Router } from "@angular/router";
 import { TranslateService } from "@ngx-translate/core";
 import { loadSTUDENT } from "../../core/state/student/student.actions";
 import { exhaustMap } from "rxjs";
+import { IPersistencePort } from "../../core/ports/persistence-port";
+import { Student } from "../../core/model/student.model";
 
 
 @Injectable()
 export class StudentEffects {
-    constructor(private action$: Actions, private service: StudentService, private route: Router, private translate: TranslateService) {
+    constructor(private action$: Actions, private service:IPersistencePort<Student,number,string,string> , private route: Router, private translate: TranslateService) {
 
     }
 
     loadStudent$ = createEffect(()=>
         this.action$.pipe(
             ofType(loadSTUDENT),
-            exhaustMap((action))
+            exhaustMap((action)=>{
+                return this.service.list(action.limit,action.offset, action.qfilter).pipe(
+                    //map(data=> loadSTUDENTsuccess({list:data})),
+                    //catchError((error)=> of(loadSTUDENTfail({errormessage:error.message})))
+                )
+            })
         )
     );
 
