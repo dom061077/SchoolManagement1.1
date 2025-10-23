0b82fdb6bc9af98b5956c9aa8fe00f84715006b0
danielmedina@synapticlinks.com.ar <danielmedina@synapticlinks.com.ar>
2025-10-23 18:58:54 -0300

    ngrx refactoring

--- src/app/presentation/student/student.module.ts
+++ src/app/presentation/student/student.module.ts
@@ -8,6 +8,9 @@ import { StoreModule } from '@ngrx/store';
 import { studentFeature } from '../../core/state/student/student-reducer';
 import { EffectsModule } from '@ngrx/effects';
 import { StudentEffects } from '../../core/state/student/student-effects';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpLoaderFactory } from '../../app.module';
 
 
 
@@ -21,7 +24,15 @@ import { StudentEffects } from '../../core/state/student/student-effects';
     StoreModule.forFeature(studentFeature.name, studentFeature.reducer),
     EffectsModule.forFeature([StudentEffects])  ,  
     MaterialModule,
-    StudentRoutingModule
+    StudentRoutingModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'es',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),    
   ]
 })
 export class StudentModule { }
--- src/app/presentation/student/studentaddedit/studentaddedit.component.ts
+++ src/app/presentation/student/studentaddedit/studentaddedit.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Student } from '../../../core/model/student.model';
+import { FormBuilder } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-studentaddedit',
@@ -6,8 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './studentaddedit.component.css'
 })
 export class StudentaddeditComponent implements OnInit {
+  title = 'Agregar Persona';
+  isedit = false;
+  dialogdata : any;
+  editcode!: number;
+  editdata!: Student;  
+
+   constructor(private builder: FormBuilder, private ref: MatDialogRef<StudentaddeditComponent>
+     ,@Inject(MAT_DIALOG_DATA) public data:any, private store: Store){
+ 
+   }  
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+
   }
+    studentForm = this.builder.group({
+      id: [''],
+      apellido: [''],
+      nombre: [''],
+      dni: [''],
+      padre: [''],  
+      madre: [''],
+      fechaNacimiento: [''],
+      fechaBautismo: [''],
+      fechaConfirmacion: [''],
+      fechaMatrimonio: [''],
+      apellidoPadrinoBaut: [''],
+      nroLibro: [''],
+      nroFolio: ['']
+    });
+
+    saveStudent(){
+
+    }
 
 }
