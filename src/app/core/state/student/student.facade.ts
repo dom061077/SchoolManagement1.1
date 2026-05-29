d1c9b9447b42cffda3a574295ab97f64a22180b3
dom061077@gmail.com <dom061077@gmail.com>
2026-05-29 14:01:58 -0300

    feat: create StudentFacade to handle student state management and search actions

--- src/app/core/state/student/student.facade.ts
+++ src/app/core/state/student/student.facade.ts
@@ -2,17 +2,17 @@ import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { FacadeBase } from '../../ngrx/facade-base';
 import { Student } from '../../model/student.model';
-import { studentCrudActions } from '../../state/student/student-actions';
+import { StudentActions } from '../../state/student/student-actions';
 import { studentSelectors } from '../../state/student/student-reducer';
 import { EntityState } from '@ngrx/entity';
 
 @Injectable({ providedIn: 'root' })
 export class StudentFacade extends FacadeBase<Student> {
   constructor(store: Store<{ feature: EntityState<Student> }>) {
-    super(store, studentCrudActions, studentSelectors);
+    super(store, StudentActions, studentSelectors);
   }
 
   searchStudents(dni: number, lastName: string, firstName: string, pageIndex: number, pageSize: number) {
-
+    this.store.dispatch(StudentActions.searchStudents({ dni, lastName, firstName, pageIndex, pageSize }));
   }
 }
